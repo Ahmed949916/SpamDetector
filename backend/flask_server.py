5ed4da499ac32487ca0b6358b725a90c6ea60b3d
Ahmed949916 <ahmed40152@gmail.com>
2024-06-10 23:59:25 +0500

    12 bje

--- backend/flask_server.py
+++ backend/flask_server.py
@@ -1,18 +1,133 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import pickle
+import re
+import pandas as pd
+from sklearn.preprocessing import StandardScaler  
+
 
 app = Flask(__name__)
-CORS(app)  
+CORS(app) 
+
+models = {
+    'LR': './Models/Logistic_Regression_feature_selection_No.pickle',
+    'DT': './Models/Decision_Tree_feature_selection_No.pickle',
+    'RF': './Models/RF.pickle',
+    'SVM': './Models/SVM.pickle',
+    'NN': './Models/NN.pickle'
+}
+
+loaded_models = {}
+for name, path in models.items():
+    name=name.replace(" ","")
+    path=path.replace(" ","")
+    print(" path=",path)
+    try:
+        with open(path, 'rb') as file:
+            loaded_models[name] = pickle.load(file)
+            
+        print(f"{name} model loaded successfully.")
+    except FileNotFoundError:
+        print(f"Error: File not found for {name}. Check the path: {path}")
+    except Exception as e:
+        print(f"Failed to load {name} due to: {e}")
+# After loading models
+print("Available models:", loaded_models.keys())
+
+col=['word_freq_make', 'word_freq_address', 'word_freq_all', 'word_freq_3d',
+       'word_freq_our', 'word_freq_over', 'word_freq_remove',
+       'word_freq_internet', 'word_freq_order', 'word_freq_mail',
+       'word_freq_receive', 'word_freq_will', 'word_freq_people',
+       'word_freq_report', 'word_freq_addresses', 'word_freq_free',
+       'word_freq_business', 'word_freq_email', 'word_freq_you',
+       'word_freq_credit', 'word_freq_your', 'word_freq_font', 'word_freq_000',
+       'word_freq_money', 'word_freq_hp', 'word_freq_hpl', 'word_freq_george',
+       'word_freq_650', 'word_freq_lab', 'word_freq_labs', 'word_freq_telnet',
+       'word_freq_857', 'word_freq_data', 'word_freq_415', 'word_freq_85',
+       'word_freq_technology', 'word_freq_1999', 'word_freq_parts',
+       'word_freq_pm', 'word_freq_direct', 'word_freq_cs', 'word_freq_meeting',
+       'word_freq_original', 'word_freq_project', 'word_freq_re',
+       'word_freq_edu', 'word_freq_table', 'word_freq_conference',
+       'char_freq_;', 'char_freq_(', 'char_freq_[', 'char_freq_!',
+       'char_freq_$', 'char_freq_#', 'capital_run_length_average',
+       'capital_run_length_longest', 'capital_run_length_total', 'spam']
+def conv(mail,columns_names,prt=False):
+    vect=[]
+    nb_word=len(re.findall(r'\w+',mail))
+    
+    #word/char freq
+    mail_low=mail.lower()
+    for col in columns_names[:-4]: # the last tree dont look for word/char freq
+        ref=col.split("_")[2] # the columns names a in the format : word/char_freq_ref
+        if ref in ["(","["]:
+            ref="\\"+ref
+        match_count=len(re.findall(f"({ref})",mail_low))
+        if prt:
+            print(ref,match_count)
+        vect.append(100*match_count/nb_word)
+    
+    # last 3 variables
+    # every sentence in capital letters
+    capital =re.findall("[A-Z, ,\d,\,]{2,}",mail)
+    longest=0
+    sum_cap=0
+    if len(capital)!=0:
+        for match in capital:
+            sum_cap+=len(match.replace(" ","")) # removing " " to get the total number of capital letter
+            size=len(match)
+            if size>longest:
+                longest=size
+    else :
+        capital=[1]
+    vect.append(sum_cap/len(capital)) # average length of uninterrupted sequences of capital letters
+    vect.append(longest) # length of longest uninterrupted sequence of capital letters
+    vect.append(sum_cap) # total number of capital letters in the e-mail
+    
+    return vect
+
 @app.route('/api', methods=['POST'])
 def handle_data():
     if request.is_json:
         data = request.get_json() 
         subjectLine = data.get('subjectLine')
         emailContent = data.get('emailContent')
+        method = data.get('method')
+        print()
+        #
+        full_text = f"{subjectLine} {emailContent}"
+        print(method)
+
+        
+        # Load the Random Forest model from the pickle file
+        model_path = './Models/Random_Forest_feature_selection_No.pickle'  # Make sure the path is correct
+        with open(model_path, 'rb') as file:
+            model = pickle.load(file)
+
+
+
+
+        email_vector=conv(full_text, col)
+        email_df = pd.DataFrame([email_vector], columns=col[:-1])
+        scaler = StandardScaler()
+        email_scaled = scaler.fit_transform(email_df)
+        prediction = model.predict(email_scaled)
+        prediction_prob = model.predict_proba(email_scaled) 
+
+        if prediction==1:
+            output='Spam with probability '+ str(prediction_prob[0][1])
+        else:
+            output='Not Spam with probability ' + str(prediction_prob[0][0])
+       
+
+
+
+
+
+        #
         print(subjectLine,emailContent)
         return jsonify({
             'message': 'Strings received!',
-            'output': "Spam"
+            'output': output
         }), 200
     else:
         return jsonify({'error': 'Request must be JSON'}), 400
--- frontend/email_spammer/src/SpamDetect.js
+++ frontend/email_spammer/src/SpamDetect.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const SpamDetect=()=> {
+const SpamDetect = () => {
   const [emailContent, setEmailContent] = useState('');
   const [subjectLine, setSubjectLine] = useState('');
+  const [classificationMethod, setClassificationMethod] = useState('Logistic Regression');
   const [classificationResult, setClassificationResult] = useState('');
   const [error, setError] = useState('');
 
@@ -15,8 +16,13 @@ const SpamDetect=()=> {
     setEmailContent(e.target.value);
   };
 
+  const handleMethodChange = (e) => {
+    console.log(e.target.value)
+    setClassificationMethod(e.target.value);
+  };
+
   const handleSubmit = async () => {
-    if (subjectLine ===''&& emailContent===''){
+    if (subjectLine === '' && emailContent === '') {
       setClassificationResult('Fields are empty');
       return;
     }
@@ -24,9 +30,8 @@ const SpamDetect=()=> {
     setError('');
 
     try {
-  
-      const response = await axios.post(` http://127.0.0.1:5000/api`, { subjectLine, emailContent });
-      console.log(response.data)
+      const response = await axios.post(`http://127.0.0.1:5000/api`, { subjectLine, emailContent, method: classificationMethod });
+      console.log(response.data);
       setClassificationResult(response.data.output);
     } catch (err) {
       setError('Failed to classify the email. Please try again.');
@@ -36,6 +41,25 @@ const SpamDetect=()=> {
   return (
     <div style={{ textAlign: 'center', backgroundColor: 'white', height: '100vh', color: '#0d1117', padding: '20px' }}>
       <h1>Spam Email Detector</h1>
+      <div>
+        <select
+          value={classificationMethod}
+          onChange={handleMethodChange}
+          style={{
+            marginBottom: '10px',
+            padding: '10px',
+            borderRadius: '5px',
+            borderColor: 'grey',
+            cursor: 'pointer'
+          }}
+        >
+          <option value="LR">Logistic Regression</option>
+          <option value="DT">Decision Tree</option>
+          <option value="RF">Random Forest</option>
+          <option value="SVM">SVM</option>
+          <option value="NN">Neural Network</option>
+        </select>
+      </div>
       <input
         value={subjectLine}
         onChange={handleSubjectLine}
@@ -46,17 +70,17 @@ const SpamDetect=()=> {
           borderColor: 'grey',
           borderWidth: '1px',
           borderStyle: 'solid',
-          width: '100%', 
+          width: '100%',
           padding: '10px',
-          borderRadius:10,
-          marginBottom:10
+          borderRadius: 10,
+          marginBottom: 10
         }}
       />
       <textarea
         value={emailContent}
         onChange={handleInputChange}
         placeholder="Paste the email content here..."
-        rows="30"
+        rows="10"
         cols="50"
         style={{
           backgroundColor: 'white',
@@ -66,7 +90,7 @@ const SpamDetect=()=> {
           borderStyle: 'solid',
           width: '100%', // To make it responsive
           padding: '10px',
-          borderRadius:10,
+          borderRadius: 10,
         }}
       ></textarea>
       <br />
@@ -74,15 +98,14 @@ const SpamDetect=()=> {
         onClick={handleSubmit}
         style={{
           textDecoration: 'none',
-          color: 'white', // White text
-          backgroundColor: '#007bff', // Bootstrap blue
+          color: 'white',
+          backgroundColor: '#007bff',
           padding: '10px 20px',
           borderRadius: '5px',
           fontSize: '1.25rem',
-          boxShadow: '0 2px 4px rgba(0,0,0,0.2)', // Subtle shadow
-          border:'none',
-          cursor:'pointer',
-          
+          boxShadow: '0 2px 4px rgba(0,0,0,0.2)',
+          border: 'none',
+          cursor: 'pointer',
         }}
       >
         Classify Email
